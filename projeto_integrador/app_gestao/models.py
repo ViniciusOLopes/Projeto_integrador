9edd4a526ee5d99301013b2890ba90f9b283f4aa
Joao Silva <joaog.oliveeira@outlook.com>
2024-10-12 14:49:03 -0300

    commit inicial

---
+++ projeto_integrador/app_gestao/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Usuarios(models.Model):
+
+    id_usuario = models.AutoField(primary_key=True)
+    nome=models.TextField(max_length=255)
+    CPF = models.TextField(max_length=255)
+    RG = models.TextField(max_length=255)
+    Orgao=models.TextField(max_length=255)
+    Emissao=models.DateField
+    Cidade_Nascimento=models.TextField(max_length=255)
+    Estado=models.TextField(max_length=255)
+    Data_Nascimento=models.DateField
+    Endereco = models.TextField(max_length=255)
+    Setor_Bairro = models.TextField(max_length=255)
+    Quadra = models.TextField(max_length=255)
+    Lote= models.TextField(max_length=255)
+    Apartamento= models.TextField(max_length=255)
+    Complemento= models.TextField(max_length=255)
+    Cidade=models.TextField(max_length=255)
+    CEP=models.IntegerField
+    Estado=models.TextField(max_length=255)
+    Telefone=models.TextField(max_length=255)
+    Email=models.TextField(max_length=255)
+    Sexo = models.TextField(max_length=255)
+    documento = models.FileField(upload_to='documentos/')
+    
+    
+
+class Curso(models.Model):
+    pass
+
+
+class SorteioCandidato(models.Model):
+    pass
+
+class MatriculaAluno(models.Model):
+    pass
